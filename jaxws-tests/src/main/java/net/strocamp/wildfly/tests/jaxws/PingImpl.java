59ba50ca06b81c72f8da5284621a4bc0a17fbd7d
Hugo Trippaers <hugo@trippaers.nl>
2014-03-17 10:34:56 +0100

    Use proper constants

--- jaxws-tests/src/main/java/net/strocamp/wildfly/tests/jaxws/PingImpl.java
+++ jaxws-tests/src/main/java/net/strocamp/wildfly/tests/jaxws/PingImpl.java
@@ -4,9 +4,10 @@ import javax.jws.WebService;
 
 import org.jboss.ws.api.annotation.EndpointConfig;
 import org.jboss.ws.api.annotation.PolicySets;
+import org.jboss.wsf.stack.cxf.extensions.policy.Constants;
 
 @WebService(endpointInterface = "net.strocamp.wildfly.tests.jaxws.Ping")
-@PolicySets({"WS-SP-EX224_WSS11_Mutual_Auth_X509_Sign_Encrypt"})
+@PolicySets({Constants.WS_SP_EX224_WSS11_Mutual_Auth_X509_Sign_Encrypt_POLICY_SET})
 @EndpointConfig(configFile = "WEB-INF/jaxws-endpoint-config-ping.xml")
 public class PingImpl implements Ping {
 
